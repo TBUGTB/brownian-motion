ad7f80701ea108f336534d1906fcf514b762e4af
Jonas Bayer <jonas.bayer999@gmail.com>
2026-04-08 10:36:12 +0200

    feat(blueprint): more details on Komlos construction

--- BrownianMotion/StochasticIntegral/ConvexWeights.lean
+++ BrownianMotion/StochasticIntegral/ConvexWeights.lean
@@ -14,13 +14,15 @@ variable {R E : Type*} [Field R] [AddCommGroup E] [Module R E]
 lemma convex_weights_of_mem_convexHull_indexed {s : ι → E} {x : E}
   (h : x ∈ convexHull R (Set.range s)) :
     ∃ (w : ι →₀ R), (∀ i, 0 ≤ w i) ∧ ∑ i : w.support, w i = 1 ∧ ∑ i : w.support, w i • s i = x := by
-  sorry
+      sorry
 
 def convexWeights (cw : ι →₀ ℝ) : Prop :=
   ∀ n : cw.support, 0 ≤ cw n ∧ ∑ n : cw.support, cw n = 1
 -- We might also choose the equivalent condition ∀ n : ι, 0 ≤ cw
 
-noncomputable def cwmul (a : ℕ →₀ ℝ) (b : ℕ → ℕ →₀ ℝ) : ℕ →₀ ℝ :=
+noncomputable section
+
+def cwmul (a : ℕ →₀ ℝ) (b : ℕ → ℕ →₀ ℝ) : ℕ →₀ ℝ :=
   Finsupp.onFinset (a.support.biUnion (fun k ↦ (b k).support))
     (fun m ↦ ∑ k ∈ a.support, a k * (b k m))
     (fun m hm ↦ by
@@ -38,7 +40,7 @@ lemma convexWeights_cwmul {a : ℕ →₀ ℝ} {b : ℕ → ℕ →₀ ℝ}
   (ha : convexWeights a) (hb : ∀ k, convexWeights (b k)) : convexWeights (cwmul a b) := by
   sorry
 
-noncomputable def cwIteratedMul (k : ℕ) (cw : ℕ → ℕ → ℕ →₀ ℝ) : ℕ → ℕ →₀ ℝ :=
+def cwIteratedMul (k : ℕ) (cw : ℕ → ℕ → ℕ →₀ ℝ) : ℕ → ℕ →₀ ℝ :=
   match k with
   | 0 => fun n ↦ cw 0 n
   | k + 1 => fun n ↦ cwmul (cw k n) (cwIteratedMul k cw)
